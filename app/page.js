a748fcb6cefc1714146277410a7728ec8dda50b0
lleung030 <lucasleung2593@gmail.com>
2023-07-20 12:55:34 -0700

    set up clone app

--- app/page.js
+++ app/page.js
@@ -1,8 +1,14 @@
 import Head from 'next/image';
 import Header from '@/components/Header';
+import SideBar from '@/components/SideBar';
 
 export default function Home() {
   return (
+    <div>
     <Header />
+    <main>
+      <SideBar />
+    </main>
+    </div>
   )
 }
