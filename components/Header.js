47f56a3d42e2841e2c7b1dac73c520b8db7fde9f
lleung030 <lucasleung2593@gmail.com>
2023-07-20 12:18:11 -0700

    active state of icons

--- components/Header.js
+++ components/Header.js
@@ -29,7 +29,7 @@ function Header() {
 
         <div className='flex justify-center flex-grow'>
             <div className='flex space-x-6 md:space-x-2'>
-    <HeaderIcon Icon={HomeIcon}/>
+    <HeaderIcon active Icon={HomeIcon}/>
     <HeaderIcon Icon={FlagIcon}/>
     <HeaderIcon Icon={PlayIcon}/>
     <HeaderIcon Icon={ShoppingCartIcon}/>
--- components/HeaderIcon.js
+++ components/HeaderIcon.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function HeaderIcon({Icon}) {
+function HeaderIcon({Icon, active}) {
   return (
     <div className='flex items-center rounded-xl active:border-b-2 active:border-blue-500 group cursor-pointer md:px-10 sm:-14 md:hover:bg-gray-100'>
-      <Icon className='h-5 group-hover:text-blue-500 text-center sm:h-7 mx-auto'/>
+      <Icon className={`h-5 text-gray-500 group-hover:text-blue-500 text-center sm:h-7 mx-auto ${active && 'text-blue-500'}`}/>
     </div>
   )
 }
